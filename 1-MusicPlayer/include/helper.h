556e14567c007f547719f98a4e9070d08af3bd25
cbedroid <cbedroid1614@gmail.com>
2021-08-26 04:43:06 -0400

    Updated 1-Musicplayer helper.h

---
+++ 1-MusicPlayer/include/helper.h
@@ -0,0 +1,9 @@
+using namespace std;
+
+const string WHITESPACE = " \n\r\t\f\v";
+string ltrim(const string &s);
+string rtrim(const string &s);
+string trim(const string &s);
+string lowerCase(const string str);
+bool isNumber(const string str);
+bool dirExists(string path);
